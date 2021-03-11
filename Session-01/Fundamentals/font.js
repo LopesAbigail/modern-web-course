3b1046d32fc5eaf1d5576232d7311085172a52e3
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-03-11 14:58:38 -0300

    Add step 01 folder

---
+++ Session-01/Fundamentals/font.js
@@ -0,0 +1,13 @@
+// Como é organizado, da forma mais basilar possível
+// Baseado em conceitos de linguagem C
+// Setenças de código -> estruturas de código
+
+console.log("Sentença de código")
+
+{
+    {
+        console.log("Hallo, Welt!")
+        console.log('Holla, que tal?')
+        console.log('Merci, beaucoup.')
+    }
+}
