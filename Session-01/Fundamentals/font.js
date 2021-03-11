d1888d99010585be486808a3b55f78e03f8c6b42
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-03-11 15:13:10 -0300

    Add var, let and const example

--- Session-01/Fundamentals/font.js
+++ Session-01/Fundamentals/font.js
@@ -1,3 +1,5 @@
+// Arquivo de experimentação dos conceitos básicos de JavaScript
+
 // Como é organizado, da forma mais basilar possível
 // Baseado em conceitos de linguagem C
 // Setenças de código -> estruturas de código
@@ -17,3 +19,14 @@ console.log("Sentença de código")
     This is a comment block
     Dies ist ein Kommentarblock
 */
+
+/*
+    ECMA Script
+    var 
+    let
+    const
+*/
+
+var example_var = 3 // Pode ser redeclarada dentro de um mesmo escopo
+let example_let = 3 // Não pode ser redeclarada dentro de um mesmo escopo
+const example_const = 4 // Não pode ter sua atribuição alterada -> constante (como o nome já diz...)
