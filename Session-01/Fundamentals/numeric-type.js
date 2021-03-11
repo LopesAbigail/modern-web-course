baebad433f843fae7d50546032d73f327f09c640
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-03-11 16:32:45 -0300

    Add Number strange properties examples

--- Session-01/Fundamentals/numeric-type.js
+++ Session-01/Fundamentals/numeric-type.js
@@ -24,5 +24,21 @@ console.log(media.toFixed(2))
 // Converte o ponto flutuante em binário
 console.log(media.toString(2))
 
-console.log(typeof media)
-console.log(typeof Number)
+console.log("media is a", typeof media)
+console.log("Number is a", typeof Number)
+
+console.log("============")
+
+// Coisas estranhas do JS
+// Tipo: infinito
+console.log(10 / 0)
+
+// Mas... não era pra ser uma string?
+// Não! Ele reconhece como um número!
+console.log("10" / 3)
+
+// Não é 0.6
+console.log(0.1 + 0.5)
+
+// Posso usar o número literal!
+console.log((0.5632).toFixed(2))
