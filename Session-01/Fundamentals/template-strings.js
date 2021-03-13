ab3c9e7332a9bbb078b19559333b8d1db642dcbe
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-03-13 00:02:55 -0300

    Add template call function example

--- Session-01/Fundamentals/template-strings.js
+++ Session-01/Fundamentals/template-strings.js
@@ -6,3 +6,9 @@ const template = `Olá,
     ${name}!`
 
 console.log(template)
+
+console.log(`1 + 5 = ${1 + 5}!`)
+
+// templates permitem concatenar chamadas de métodos!
+const up = text => text.toUpperCase()
+console.log(`Ei... ${up('cuidado')}`)
