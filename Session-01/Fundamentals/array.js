cf65026cb8c3e259507583d3a6947f16a8f21d30
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-03-26 14:32:22 -0300

    Add array examples comments

--- Session-01/Fundamentals/array.js
+++ Session-01/Fundamentals/array.js
@@ -1,17 +1,18 @@
-// Vetores
+// Vetores heterogeneos
 const valores = [7.7, 8.9, 6.3, 9.2]
 console.log(valores[0], valores[3])
-console.log(valores[4])
+console.log(valores[4]) // undefined
 
 valores[4] = 10
-console.log(valores)
+console.log(valores) // add index 4
 console.log(valores.length)
 
-valores.push({ id: 3 }, false, null, 'teste')
+valores.push({ id: 3 }, false, null, 'teste') // array beem heterogeneo --> eh melhor trabalhar com homogeneos, tem-se um codigo mais organizado
 console.log(valores)
 
-console.log(valores.pop())
-delete valores[0]
+console.log(valores.pop()) // elimina o ultimo item do array e retorna esse valor
+console.log(valores)
+delete valores[0] // retira elemento, deixando-o vazio
 console.log(valores)
 
 console.log(typeof valores)
