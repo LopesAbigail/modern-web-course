dc42e23ce775e5f654da66b6d9e90b92e577211f
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-03-26 15:25:51 -0300

    Add functions examples comments

--- Session-01/Fundamentals/functions.js
+++ Session-01/Fundamentals/functions.js
@@ -9,10 +9,11 @@ imprimirSoma(2, 10, 4, 5, 6, 7, 8)
 imprimirSoma()
 
 // Funcao com retorno
-function soma(a, b = 1) {
+// valor padrao de b eh 1
+function somar(a, b = 1) {
     return a + b
 }
 
-console.log(soma(2, 3))
-console.log(soma(2))
-console.log(soma())
+console.log(somar(2, 3))
+console.log(somar(2))
+console.log(somar())
