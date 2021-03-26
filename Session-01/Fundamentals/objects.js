70b28bcdb21500cc5e8389f0776c2caa0e9db54b
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-03-26 14:39:35 -0300

    Add objects examples comments

--- Session-01/Fundamentals/objects.js
+++ Session-01/Fundamentals/objects.js
@@ -1,5 +1,6 @@
+// colecao de chave e valor
 const prod1 = {}
-prod1.nome = 'Celular Ultra Mega'
+prod1.nome = 'Celular Ultra Mega' // criacao dinamica
 prod1.preco = 4998.90
 prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço
 
@@ -10,4 +11,14 @@ const prod2 = {
     preco: 79.90
 }
 
+const prod3 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    fornecedor: {
+        nome: "Examplelina",
+        endereco: "Rua da Lua"
+    }
+}
+
 console.log(prod2)
+console.log(prod3)
