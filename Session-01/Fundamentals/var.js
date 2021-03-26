5ba919a27e1bdb84f5dc4ffa4c1e38b1cb1d8cba
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-03-26 15:44:11 -0300

    Add var declaration example details

--- Session-01/Fundamentals/var.js
+++ Session-01/Fundamentals/var.js
@@ -1,3 +1,4 @@
+  // var fica visivel em TODO o escopo do codigo
   {
       {
           {
@@ -11,10 +12,21 @@
 
   console.log(sera)
 
+  // contudo, em um contexto de funcao, a var esta definida apenas em seu escopo
   function teste() {
       var local = 123
       console.log(local)
   }
 
   teste()
-  console.log(local)
+      //console.log(local)
+
+  // Evitar variaveis que vao para o escopo global - sobrescrever
+  var numero = 1
+
+  {
+      var numero = 2
+      console.log('dentro =', numero)
+  }
+
+  console.log('fora =', numero)
