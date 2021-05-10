1276f5679b8b550183881694a92ce62080443bcd
LopesAbigail <contatoabigail_lopes@outlook.com>
2021-05-10 20:48:03 -0300

    Add params properties and examples

--- Session-01/Fundamentals/first-class-citizens.js
+++ Session-01/Fundamentals/first-class-citizens.js
@@ -25,3 +25,40 @@ function doSum(a, b) {
 }
 
 doSum(1, 3)(4)
+
+// Params, arguments (intern array)
+function subtraction() {
+    let subtraction = 0
+    for (i in arguments) {
+        subtraction -= arguments[i]
+    }
+    return subtraction
+}
+
+console.log(subtraction(4, 9, 2, 1, 7, 4, 3, 2, 1.32))
+
+// Default param
+// Before ECMA Script 2015
+// it doesn't work with zeros :(
+function sumDefaultParam_before(a, b, c) {
+    a = a || 1
+    b = b || 1
+    c = c || 1
+
+    return a + b + c
+}
+console.log(sumDefaultParam_before())
+console.log(sumDefaultParam_before(4, 5))
+
+function sumDefaultParam_before_other_examples(a, b, c) {
+    a = a !== undefined ? a : 1
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c // the most secure
+
+    return a + b + c
+}
+
+// after
+function sumDefaultParam_after(a = 1, b = 1, c = 1) {
+    return a + b + c
+}
